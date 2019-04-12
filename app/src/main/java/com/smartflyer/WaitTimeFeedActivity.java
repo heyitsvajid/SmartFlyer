b8eba7f3d8c1c7e00fbfdb8dac89396d1207632d
Zenobia <zenobiapanvelwalla@gmail.com>
2019-04-11 20:00:53 -0700

    Half way through calling API.

--- app/src/main/java/com/smartflyer/WaitTimeFeedActivity.java
+++ app/src/main/java/com/smartflyer/WaitTimeFeedActivity.java
@@ -5,11 +5,30 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+import com.loopj.android.http.*;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
 
 public class WaitTimeFeedActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    TextView airportName;
+    TextView terminal;
+    EditText noOfPeople;
+    EditText noOfActiveQueues;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -27,6 +46,7 @@ public class WaitTimeFeedActivity extends AppCompatActivity {
                     mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
+
             return false;
         }
     };
@@ -36,9 +56,49 @@ public class WaitTimeFeedActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wait_time_feed);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+         airportName = (TextView)findViewById(R.id.airport_name);
+         terminal = (TextView)findViewById(R.id.terminal);
+         noOfActiveQueues = (EditText)findViewById(R.id.no_of_active_queues);
+         noOfPeople = (EditText)findViewById(R.id.no_of_people_before);
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+
+    public void feedData(View v){
+        System.out.println("Method CALLED!!!");
+        final RequestQueue queue = Volley.newRequestQueue(this);
+        final String url = "http://localhost:3000/wait-times/insert"; // your URL
+
+        queue.start();
+
+                HashMap<String, String> params = new HashMap<String,String>();
+                params.put("aiportName", airportName.getText().toString()); // the entered data as the body.
+                params.put("terminal",terminal.getText().toString());
+                params.put("noOfActiveQueues",noOfActiveQueues.getText().toString());
+                params.put("noOfPeopleBefore",noOfPeople.getText().toString());
+
+                JsonObjectRequest jsObjRequest = new
+                        JsonObjectRequest(Request.Method.POST,
+                        url,
+                        new JSONObject(params),
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                try {
+                                    Toast.makeText(WaitTimeFeedActivity.this,response.getString("message"),Toast.LENGTH_SHORT);
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(WaitTimeFeedActivity.this,error.getMessage().toString(),Toast.LENGTH_SHORT);
+                    }
+                });
+                queue.add(jsObjRequest);
+            }
+
 
 }
