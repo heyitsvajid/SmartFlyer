ee6df8bb60feffeafc803b8d560f0ee6933aad70
Vajid Kagdi <vajid.kagdi@sjsu.edu>
2019-04-15 20:56:56 -0700

    Airport Search functionality implemented

--- app/src/main/java/com/smartflyer/SearchAirport.java
+++ app/src/main/java/com/smartflyer/SearchAirport.java
@@ -1,21 +1,31 @@
 package com.smartflyer;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.app.Activity;
-import android.os.Looper;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -32,6 +42,15 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.gson.JsonArray;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import static com.google.android.gms.location.LocationServices.getFusedLocationProviderClient;
 
@@ -44,13 +63,30 @@ public class SearchAirport extends Activity {
     private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
     private static final String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
 
-
+    private RecyclerView mRecyclerView;
+    private ArrayList<Airport> mSportsData;
+    private AirportsAdapter mAdapter;
+    private EditText edit_txt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_airport);
-        getDeviceLocation();
 
+        // Initialize the RecyclerView.
+        mRecyclerView = findViewById(R.id.recyclerView);
+
+        // Set the Layout Manager.
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // Initialize the ArrayList that will contain the data.
+        mSportsData = new ArrayList<>();
+
+        // Initialize the adapter and set it to the RecyclerView.
+        mAdapter = new AirportsAdapter(this, mSportsData);
+        mRecyclerView.setAdapter(mAdapter);
+
+        // Get the location.
+        getDeviceLocation();
     }
 
     private void getDeviceLocation() {
@@ -69,6 +105,10 @@ public class SearchAirport extends Activity {
                             Location currentLocation = (Location) task.getResult();
                             System.out.println("LOOK AT  "+currentLocation);
                             Toast.makeText(getApplicationContext(), currentLocation.toString(), Toast.LENGTH_SHORT).show();
+                            HashMap<String, String> params = new HashMap<String,String>();
+                            params.put("lat", String.valueOf(currentLocation.getLatitude())); // the entered data as the body.
+                            params.put("lon", String.valueOf(currentLocation.getLongitude())); // the entered data as the body.
+                            callAirportsApi(params,"airport/getNearest");
                             //Snackbar.make(rootView,"You can now search cafes",Snackbar.LENGTH_SHORT).show();
 
                         }else{
@@ -104,21 +144,17 @@ public class SearchAirport extends Activity {
         }
     }
 
-//    @Override
-//    public void onConnected(@Nullable Bundle bundle) {
-//
-//    }
-//
-//    @Override
-//    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-//
-//    }
-//
-//    @Override
-//    public void onConnectionSuspended(int i) {
-//
-//    }
-
+    public void onSearch(View v){
+        edit_txt = (EditText) findViewById(R.id.input_search);
+        String s = edit_txt.getText().toString().trim();
+        if (s == null || s.length() == 0) {
+            Toast.makeText(getApplicationContext(), "Please enter text.", Toast.LENGTH_SHORT).show();
+        } else
+            Toast.makeText(getApplicationContext(), "Searching....", Toast.LENGTH_SHORT).show();
+            HashMap<String, String> params = new HashMap<String,String>();
+            params.put("query", String.valueOf(s)); // the entered data as the body.
+            callAirportsApi(params,"airport/search");
+    }
 
     private void getLocationPermission(){
 
@@ -141,5 +177,70 @@ public class SearchAirport extends Activity {
                     permissions,
                     LOCATION_PERMISSION_REQUEST_CODE);
         }
+    }
+
+    public List<Airport> callAirportsApi(final HashMap<String, String> params ,final String url){
+        List<Airport> result = new ArrayList<Airport>();
+        final Context con = this;
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        final String URL = Config.BACKEND_URL + url; // your URL
+                        final RequestQueue queue = Volley.newRequestQueue(con);
+                        queue.start();
+                        JsonObjectRequest jsObjRequest = new
+                                JsonObjectRequest(Request.Method.POST,
+                                URL,
+                                new JSONObject(params),
+                                new Response.Listener<JSONObject>() {
+                                    @Override
+                                    public void onResponse(JSONObject response) {
+                                        try {
+                                            System.out.println("Success Reponse");
+                                            JSONArray arr = (JSONArray)response.get("data");
+                                            setData(arr);
+                                            if(arr.length() > 0){
+                                                Toast.makeText(getApplicationContext(), "Airport list updated.", Toast.LENGTH_SHORT).show();
+                                            }else{
+                                                Toast.makeText(getApplicationContext(), "No airports found.", Toast.LENGTH_SHORT).show();
+                                            }
+                                        } catch (JSONException e) {
+                                            System.out.println("Inner NOT WORKING!!" + e.getMessage());
+                                            e.printStackTrace();
+                                        }
+                                    }
+                                }, new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                System.out.println("Outer NOT WORKING!!"+ error.getMessage());
+                            }
+                        });
+                        queue.add(jsObjRequest);
+                    }
+                }, 3000);
+        return result;
+    }
+
+
+    /**
+     * Initialize the sports data from resources.
+     */
+    private void setData(JSONArray arr) {
+        mSportsData.clear();
+        try {
+            for (int i = 0; i < arr.length(); i++) {
+                System.out.println(arr.get(i));
+                JSONObject o = (JSONObject)arr.get(i);
+                Airport a = new Airport(o.getString("_id"),o.getString("name"),
+                        o.getString("city"),o.getString("country"),o.getString("iata"),
+                        o.getString("latitude"),o.getString("longitude"));
+                mSportsData.add(a);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        // Notify the adapter of the change.
+        mAdapter.notifyDataSetChanged();
     }
 }
